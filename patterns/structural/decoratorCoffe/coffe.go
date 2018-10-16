6d30d15b9be0184eb24b4b429a49483efa8885e9
VadimJedi <108vadim@mail.ru>
2018-10-16 22:56:42 +0300

    decorator coffe

---
+++ patterns/structural/decoratorCoffe/coffe.go
@@ -0,0 +1,66 @@
+package main
+
+import "fmt"
+
+type Costing interface {
+	Cost() uint
+}
+
+type coffeBlack struct {
+}
+
+func NewCoffeBlack() *coffeBlack {
+	return &coffeBlack{}
+}
+
+func (b *coffeBlack) Cost() uint {
+	return 100
+}
+
+type coffeGreen struct{}
+
+func NewCoffeGreen() *coffeGreen {
+	return &coffeGreen{}
+}
+
+func (g *coffeGreen) Cost() uint {
+	return 200
+}
+
+type sugar struct {
+	parent Costing
+}
+
+func NewSugar(cmp Costing) Costing {
+	return &sugar{
+		parent: cmp,
+	}
+}
+
+func (g sugar) Cost() uint {
+	return g.parent.Cost() + 15
+}
+
+type milk struct {
+	parent Costing
+}
+
+func NewMilk(cmp Costing) Costing {
+	return &milk{
+		parent: cmp,
+	}
+}
+
+func (g milk) Cost() uint {
+	return g.parent.Cost() + 15
+}
+
+func main() {
+
+	var coffe Costing = NewCoffeBlack()
+	addid := NewSugar(coffe)
+	addid = NewMilk(addid)
+	cost := addid.Cost()
+	fmt.Println(cost)
+
+}
